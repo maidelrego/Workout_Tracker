f23caa254dace6f9616fec9d12354850d976732f
Legend <maesabroso@gmail.com>
2020-04-12 11:29:40 -0500

    working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Workout_Tracker
+# Fitness Tracker
+Allows the user to track a workout. Various attributes of the workout can be stored. The workouts are saved in a Mongo database.
+
+## Description 
+Created a fitness tracking app utilizing Node.js, Express.js, MongoDB, Heroku and Mongoose. Majority of the front end code was provided in a homework prompt as part of a web development boot camp. Allows the user to track their workouts and save them in a database.
+
+![](gif.gif)
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// routes
-app.use(require("./routes/api.js"));
+const db = require("./models");
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
